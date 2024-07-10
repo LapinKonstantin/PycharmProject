eaae68552e96a06f2f4b14c34782dfd13ebdecbe
LapinKonstantin <knight892016@mail.ru>
2024-07-10 23:40:06 +0500

    str_func.py

--- str_func.py
+++ str_func.py
@@ -1,13 +1,10 @@
 import string
 
-Konstantin +1
-
 def uppercase_string(input_string):
-    ###Функция выводит строку с заглавными буквами###
+    "Функция выводит строку с заглавными буквами"
     return input_string.upper()
 
-Konstantin
 
-def capital_letters(input_string)
-    ###Функция делает заглавными первые буквы каждого слова в строке, подсупившей на вход функции###
-/bin/bash: line 1: q: command not found
+def capital_letters(input_string):
+    "Функция делает заглавными первые буквы каждого слова в строке, подсупившей на вход функции"
+    return capital_letters()
