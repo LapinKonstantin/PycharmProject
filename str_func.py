dbd912f655c99dcd13168cbf1fa47c66824aaab0
LapinKonstantin <knight892016@mail.ru>
2024-07-10 20:33:38 +0500

    str_func.py

---
+++ str_func.py
@@ -0,0 +1,13 @@
+import string
+
+Konstantin +1
+
+def uppercase_string(input_string):
+    ###Функция выводит строку с заглавными буквами###
+    return input_string.upper()
+
+Konstantin
+
+def capital_letters(input_string)
+    ###Функция делает заглавными первые буквы каждого слова в строке, подсупившей на вход функции###
+/bin/bash: line 1: q: command not found
